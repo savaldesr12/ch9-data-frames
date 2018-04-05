8afefcf57e9b37b2952794a4dec46fa2ea7197a5
Samuel Valdes <samuelvaldes@D-108-179-157-83.dhcp4.washington.edu>
2018-04-04 17:02:05 -0700

    Completes ch9 exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,28 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+scored <- c(24, 24, 24, 7)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+allowed <- c(13, 10, 30, 42)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- data.frame(scored, allowed)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+games$diff <- games$scored - games$allowed
 
 # Create a new column "won" which is TRUE if the Seahawks won the game
-
+games$won <- games$diff > 0
 
 # Create a vector of the opponent names corresponding to the games played
-
+opponents <- c("49ers", "Eagles", "Jaguars", "Rams")
 
 # Assign your dataframe rownames of their opponents
-
+rownames(games) <- opponents
 
 # View your data frame to see how it has changed!
 
