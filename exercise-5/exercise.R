9e348fb123c6a08a1a615b05019856d2590078d7
Samuel Valdes <samuelvaldes@D-69-91-137-39.dhcp4.washington.edu>
2018-04-09 20:12:48 -0700

    Completes exercise 5

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -2,32 +2,37 @@
 
 # Read in the female baby names data file found in the `data` folder into a 
 # variable called `names`. Remember to NOT treat the strings as factors!
-
+names <- read.csv("data/female_names.csv", stringsAsFactors = F)
 
 # Create a data frame `names_2013` that contains only the rows for the year 2013
-
+names_2013 <- names[names$year == "2013",]
 
 # What was the most popular female name in 2013?
-
+most_popular <- names_2013$name[names_2013$prop == max(names_2013$prop)]
 
 # Write a function `most_popular_in_year` that takes in a year as a value and 
 # returns the most popular name in that year
-
+most_popular_in_a_year <- function(year) {
+  names_year <- data.frame(names[names$year == year,])
+  names_year$name[names_year$prop == max(names_year$prop)]
+}
 
 # What was the most popular female name in 1994?
-
+pop_1994 <- most_popular_in_a_year(1994)
 
 # Write a function `number_in_million` that takes in a name and a year, and 
 # returns statistically how many babies out of 1 million born that year have 
 # that name. 
 # Hint: get the popularity percentage, and take that percentage out of 1 million.
-
+number_in_million <- function(name, year) {
+  names$prop[names$name == name & names$year == year] * 1000000
+}
 
 # How many babies out of 1 million had the name 'Laura' in 1995?
-
+laura <- number_in_million("Laura", 1995)
 
 # How many babies out of 1 million had your name in the year you were born?
-
+samuel <- number_in_million("Samuel", 1999)
 
 ## Consider: what does this tell you about how easy it is to identify you with 
 ## just your name and birth year?
